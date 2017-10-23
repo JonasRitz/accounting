8e945fb130546de1a38ced99382f5210b1970cbe
jonasritz <j.ritz@fz-juelich.de>
2017-10-23 16:53:24 +0200

    geile Kleine Version

--- src/application/accounting/Accounting.java
+++ src/application/accounting/Accounting.java
@@ -4,12 +4,22 @@ import java.io.*;
 public class Accounting{
   public static double genauigkeit = 1000000.0;
 
-  public static void main(String argv[]){
-    Scanner sc = new Scanner(System.in);
+  public static void main(String argv[]) throws FileNotFoundException {
+    String datei;
+    String zins;
+    if(argv.length != 0){
+        ArgParser p1 = new ArgParser(argv);
+        datei = p1.getInputFilename();
+        zins = p1.getNonOptions().replace(",",".");
+        Depositor.setzeZinsen(Double.parseDouble(zins));
+    }else{
+        Scanner sc = new Scanner(System.in);
+        datei = sc.nextLine();
+        zins  = sc.nextLine();
+        Depositor.setzeZinsen(Double.parseDouble(zins));
+    }
+    
     ArrayList<Depositor> mitglieder = new ArrayList<>();
-    String datei = sc.nextLine();
-    String zins  = sc.nextLine();
-    Depositor.setzeZinsen(Double.parseDouble(zins));
     try(BufferedReader br = new BufferedReader(new FileReader(datei))){
       String line;
       while( (line=br.readLine()) != null){
@@ -31,6 +41,7 @@ public class Accounting{
       }
 
     }catch(IOException e){
+      e.printStackTrace();
       System.out.println("Datei nicht gefunden");
     }
     for(Depositor dep : mitglieder){
